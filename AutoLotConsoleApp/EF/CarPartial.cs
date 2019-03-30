abc58f0c97a2f441cfb69974d37442a49c581886
Рафаэль Хасанов <rap1raf@gmail.com>
2019-03-31 01:14:26 +0300

    SomeCodeADD

---
+++ AutoLotConsoleApp/EF/CarPartial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoLotConsoleApp.EF
+{
+    public partial class Car
+    {
+        public override string ToString()
+        {
+            //Поскольку столбец PetName может быть пустым предоставить стандартное имя NoName
+            return $"{this.CarNickName ?? "NoNam"} is a {this.Color}, {this.Make} with ID {this.CarId}.";
+        }
+    }
+}
--- AutoLotConsoleApp/Program.cs
+++ AutoLotConsoleApp/Program.cs
@@ -10,6 +10,7 @@ namespace AutoLotConsoleApp
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
